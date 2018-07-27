9087af1df7ac760e84ead3e534cbfe946819b080
Mr. Sarful hassan <36908730+sarful@users.noreply.github.com>
2018-07-27 12:18:31 +0600

    Add files via upload

---
+++ rain_sensor.ino.ino
@@ -0,0 +1,90 @@
+
+/*
+
+
+
+Agu 2018
+Author: Sarful hassan
+Website: https://mechatronicslabrpi.blogspot.com
+
+*/ 
+
+
+
+//library esp
+#include <ESP8266WiFi.h>
+const char* ssid = "rowsoni";                                  //fill in your wifi name
+const char* password = "007892057";                              //fill in your wifi password
+
+
+WiFiClient client;
+
+int sensorPin = A0;    // input for LDR and rain sensor
+int enable2 = 13;      // enable reading Rain sensor
+int sensorValue2 = 0;  // variable to store the value coming from sensor Rain sensor
+
+
+//--------------------------setup-------------------------
+void setup() {
+
+// declare the enable and ledPin as an OUTPUT:
+pinMode(enable2, OUTPUT);
+
+  
+Serial.begin(115200);
+delay(10);
+
+
+WiFi.begin(ssid, password);
+
+Serial.println();
+Serial.println();
+Serial.print("Connecting to ");
+Serial.println(ssid);
+Serial.print("..........");
+Serial.println();
+WiFi.begin(ssid, password);
+
+while (WiFi.status() != WL_CONNECTED) {
+delay(500);
+
+}
+Serial.println("WiFi connected");
+Serial.println();
+
+}
+
+
+void loop() {
+
+
+
+//--------------------------Rain Sensor-------------------------
+
+
+
+delay(500);
+sensorValue2 = analogRead(sensorPin);
+sensorValue2 = constrain(sensorValue2, 150, 440); 
+sensorValue2 = map(sensorValue2, 150, 440, 1023, 0); 
+if (sensorValue2>= 20)
+{
+  Serial.print("rain is detected");
+  
+digitalWrite(enable2, HIGH);
+  }
+  else
+  
+{
+  Serial.print("rain not detected");
+  digitalWrite(enable2, LOW); 
+  }
+//Serial.print("Rain value:       ");
+//Serial.println(sensorValue2);
+Serial.println();
+delay(100);
+
+
+
+}
+
